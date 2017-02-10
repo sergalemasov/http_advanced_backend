f982e7540762ce3c57256005b2f4bae2bb123ffb
Sergei Alemasov <sergei_alemasov@epam.com>
2017-02-11 03:05:40 +0400

    start implement img renamer

---
+++ main-server/services/img-renamer.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+var ImgRenamer = function () {
+  this.active = false;
+  this.oldFilePath = null;
+};
+
+var _proto = ImgRenamer.prototype;
+
+_proto.start = function () {
+  setTimeout()
+};
+
+_proto.rename = function() {
+
+}
+
+_proto.imgList = ['1.jpg'];
