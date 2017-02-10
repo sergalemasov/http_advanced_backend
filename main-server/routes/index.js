8207d18deffa9ea8621819959e4bfe7a5c6889ea
Sergei Alemasov <sergei_alemasov@epam.com>
2017-02-11 02:01:27 +0400

    add basic and digest auth handlers

--- main-server/routes/index.js
+++ main-server/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('../middlewares/passport-auth');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get(
+  '/', 
+  passport.authenticate('basic', {'session': false}), 
+  function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  }
+);
 
 module.exports = router;
