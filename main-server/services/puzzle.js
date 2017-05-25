d8275e0b6ebaf5af1c6171f5b14577c4ce712501
Sergei Alemasov <Sergei_Alemasov@epam.com>
2017-05-25 22:36:48 +0400

    some fixes with puzzle.js

--- main-server/services/puzzle.js
+++ main-server/services/puzzle.js
@@ -17,9 +17,8 @@ _proto.getHeaders = function (num, server) {
   switch (+num) {
     case 1:
     case 5:
-    case 9:
     case 4:
-    case 6:
+    case 9:
       return {
         'Access-Control-Allow-Origin': server,
         'Access-Control-Expose-Headers': 'src',
@@ -30,6 +29,12 @@ _proto.getHeaders = function (num, server) {
     case 3:
       return {
         'Access-Control-Allow-Origin': server,
+        'src': this.getImgUrl(num),
+      }
+    case 8:
+    case 6:
+      return {
+        'Access-Control-Expose-Headers': 'src',
         'src': this.getImgUrl(num),
       }
   }
